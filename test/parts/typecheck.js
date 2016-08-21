85f2b6132c49f9252f7fb90c1add327a988dc729
Adrian Toncean <adrianton3@gmail.com>
2016-08-21 22:42:35 +0200

    Update unit tests for type checking

--- test/parts/typecheck.js
+++ test/parts/typecheck.js
@@ -1,52 +1,27 @@
 (function() {
 	"use strict";
 	
-	var Tokenizer = require('../tokenizer/Tokenizer.js').Tokenizer;
-	var TokenCoords = require('../tokenizer/TokenCoords.js').TokenCoords;
-	
-	var Add = require('../interpreter/nodes/Add.js').Add;
-	var And = require('../interpreter/nodes/And.js').And;
-	var Bool = require('../interpreter/nodes/Bool.js').Bool;
-	var BoolQ = require('../interpreter/nodes/BoolQ.js').BoolQ;
-	var Call = require('../interpreter/nodes/Call.js').Call;
-	var ClosureQ = require('../interpreter/nodes/ClosureQ.js').ClosureQ;
-	var ContainsQ = require('../interpreter/nodes/ContainsQ.js').ContainsQ;
-	var Def = require('../interpreter/nodes/Def.js').Def;
-	var Div = require('../interpreter/nodes/Div.js').Div;
-	var Fst = require('../interpreter/nodes/Fst.js').Fst;
-	var Fun = require('../interpreter/nodes/Fun.js').Fun;
-	var If = require('../interpreter/nodes/If.js').If;
-	var Let = require('../interpreter/nodes/Let.js').Let;
-	var Mod = require('../interpreter/nodes/Mod.js').Mod;
-	var Module = require('../interpreter/Module.js').Module;
-	var ModuleSet = require('../interpreter/ModuleSet.js').ModuleSet;
-	var Mul = require('../interpreter/nodes/Mul.js').Mul;
-	var Not = require('../interpreter/nodes/Not.js').Not;
-	var Num = require('../interpreter/nodes/Num.js').Num;
-	var NumQ = require('../interpreter/nodes/NumQ.js').NumQ;
-	var Or = require('../interpreter/nodes/Or.js').Or;
-	var Pair = require('../interpreter/nodes/Pair.js').Pair;
-	var PairQ = require('../interpreter/nodes/PairQ.js').PairQ;
-	var Record = require('../interpreter/nodes/Record.js').Record;
-	var RecordQ = require('../interpreter/nodes/RecordQ.js').RecordQ;
-	var Snd = require('../interpreter/nodes/Snd.js').Snd;
-	var Str = require('../interpreter/nodes/Str.js').Str;
-	var StrQ = require('../interpreter/nodes/StrQ.js').StrQ;
-	var Sub = require('../interpreter/nodes/Sub.js').Sub;
-	var Unit = require('../interpreter/nodes/Unit.js').Unit;
-	var UnitQ = require('../interpreter/nodes/UnitQ.js').UnitQ;
-	var Var = require('../interpreter/nodes/Var.js').Var;
-	var Xor = require('../interpreter/nodes/Xor.js').Xor;
-	
 	var Env = require('../interpreter/Env.js').Env;
 	
 	var StaticCheck = require('../interpreter/StaticCheck.js').StaticCheck;
 	var VarCheckState = require('../interpreter/VarCheckState.js').VarCheckState;
 	
-	var RDP = require('../parser/RDP.js').RDP;
 	var Type = require('../interpreter/types/Type.js').Type;
-	
-	var _t = function(s) { return RDP.single(Tokenizer.chop(s)).accept(new StaticCheck(), new VarCheckState(Env.Emp, null)); };
+
+	const { buildAst } = require('../../src/ast/AstBuilder.js').AstBuilder
+
+	function parse (source) {
+		const tokens = espace.Tokenizer()(source)
+		const rawTree = espace.Parser.parse(tokens)
+		return buildAst(rawTree)
+	}
+
+	function _t (source) {
+		return parse(source).accept(
+			new StaticCheck(),
+			new VarCheckState(Env.Emp, null)
+		)
+	}
 
 	var _tvid     = new Type(false, false, false, false, false, false, false, false);
 	var _tany     = new Type( true, false, false, false, false, false, false, false);
@@ -60,6 +35,7 @@
 	var _tfun_num = new Type(false, false, false, false, false, false, false, _tnum);
 
 	module('type checker');
+
 	test('Basic type eval', function() {
 		ok(_t('#f').equals(_tbool), 'TypeBool');
 		ok(_t('234').equals(_tnum), 'TypeNum');
